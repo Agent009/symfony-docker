18c61c9bd1f539699e8c2c8c0188e4649cd33588
Mohammad Amir <amir@connextar.com>
2024-04-22 01:59:21 +0100

    Added basic packages such as profiler, logger and mailer.

---
+++ src/EventSubscriber/SymfonyDebugSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class SymfonyDebugSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        // return the subscribed events, their methods and priorities
+        return [
+            KernelEvents::EXCEPTION => [
+                ['onKernelResponse', 0],
+            ],
+        ];
+    }
+
+    // ...
+
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        if (!$this->kernel->isDebug()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        if (!$request->isXmlHttpRequest()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+        $response->headers->set('Symfony-Debug-Toolbar-Replace', 1);
+    }
+}
